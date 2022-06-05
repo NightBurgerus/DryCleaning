084da8cb745d29b8ad3293437eb3d3468e050f15
NightBurgerus <terehov.pasha@yandex.ru>
2022-06-05 14:20:37 +0300

    Fix form

--- scripts/formScript.js
+++ scripts/formScript.js
@@ -1,13 +1,6 @@
-/*
-    TODO:
-        - Добавить регулярку на телефон
-        - Добавить вывод сообщения о невалидном поле
-        - Сделать кнопку сброс
-
-*/
-
 let productList = document.getElementsByClassName("productList")[0]
-let addButton   = document.getElementsByClassName("product")[0].lastChild
+let addButton   = document.getElementsByClassName("addButton")[0].lastChild
+
 
 let clothes     = ["Повседневная", "Торжественная", "Верхняя", "Детская", "Домашняя", "Спортивная"]
 let shoes       = ["Туфли", "Сапоги", "Кроссовки", "Ботинки", "Балетки"]
@@ -23,41 +16,47 @@ function contains(arr, elem) {
     return false;
 }
 
-function delCategory(i) {
-    category = productList.getElementsByTagName("div")[i]
-    mainList = category.getElementsByTagName("input")[0]
-    secondList = category.getElementsByTagName("input")[1]
-    delButton  = category.getElementsByTagName("div")[0]
+function delCategory(button) {
 
-    if (productList.children.length < 2) {
-        mainList.value = ""                 // Очистка основной категории
-        category.removeChild(secondList)    // Удаление уточняющего списка 
-        category.removeChild(delButton)     // Удаление кнопки удаления
+    let div = button.parentElement
+    let parent = div.parentElement
+    
+    if (parent.children.length < 2) {
+        div.getElementsByTagName("input")[0].value = ""
+        div.removeChild(div.getElementsByTagName("input")[1])
+        div.removeChild(div.getElementsByClassName("delButton")[0])
     } else {
-        productList.removeChild(category)   // Полное удаление строки
+        parent.removeChild(div)
     }
-    productList.style.height = productList.children.length * productList.firstElementChild.clientHeight + "px";
 }
+
+
 
 function addEvent(product, i) {
     let input = product.getElementsByTagName("input")[0]
     input.addEventListener("input", function() {
-        //let delButton = document.createElement("input")
         let delButton = document.createElement("div")
         let field= document.createElement("input")
 
         // Добавление атрибута list для пытадающего списка
         if (input.value == "Одежда") {
             field.setAttribute("list", "clothes")
+            field.setAttribute("name", "clothes")
         } else if (input.value == "Обувь") {
             field.setAttribute("list", "shoes")
+            field.setAttribute("name", "shoes")
         } else if (input.value == "Домашний интерьер") {
             field.setAttribute("list", "home")
+            field.setAttribute("name", "home")
         } else if (input.value == "Аксессуары") {
             field.setAttribute("list", "accessories")
+            field.setAttribute("name", "accessories")
         } else {
             return;
         }
+
+        field.classList.add("form__input")
+        field.classList.add("_req")
 
         // Если категория изменилась, нужно удалить уточняющий список 
         // прошлой категории
@@ -74,8 +73,16 @@ function addEvent(product, i) {
                 || contains(accessories, field.value) || contains(home, field.value)) {
                 delButton.classList.add("delButton")
                 delButton.addEventListener("click", function() {
-                    delCategory(i)
+                    delCategory(delButton)
                 })
+                delButton.classList.add("shadow")
+                delButton.style.marginTop = "5px"
+                delButton.style.height = "20px"
+                delButton.style.paddingTop = "5px"
+                delButton.style.paddingBottom = "5px"
+                delButton.style.width = "80px"
+                delButton.style.textAlign = "center"
+                delButton.textContent = "Удалить"
                 product.appendChild(delButton)
             }
         })
@@ -89,9 +96,11 @@ function addCategory() {
             let input = document.createElement("input")
             input.setAttribute("list", "products")
             div.appendChild(input)
+            input.classList.add("form__input")
+            input.classList.add("_req")
+            input.setAttribute("onkeyup", "inputChanged(this)")
             productList.appendChild(div)
             addEvent(div)
-            productList.style.height = productList.children.length * productList.firstElementChild.clientHeight + "px";
     }
 }
 
@@ -119,11 +128,17 @@ number.addEventListener("keyup", function(e) {
 
 
 function validateDate() {
-    let inputDate   = document.getElementsByClassName("date")[0].getElementsByTagName("input")[0].valueAsDate.toString()
-    let currentDate = Date().toString()
-    currentDate = currentDate.match(/^.*\s\d{4}/i).pop()
-    inputDate   = inputDate.match(/^.*\s\d{4}/i).pop()
-    console.log(inputDate < currentDate)
+    setTimeout(function() {
+        let inputDate   = document.getElementsByClassName("date")[0].valueAsDate.toString()
+        let currentDate = Date().toString()
+        currentDate = currentDate.match(/^.*\s\d{4}/i).pop()
+        inputDate   = inputDate.match(/^.*\s\d{4}/i).pop()
+        if (inputDate < currentDate) {
+            alert("Введена некорректная дата")
+            return false
+        }
+        return true
+    }, 4000)
 }
 
 function submitClicked() {
@@ -139,4 +154,16 @@ function submitClicked() {
     alert("Данные отправлены на сервер")
     console.log(true)
     return true
+}
+
+function inputChanged(sender) {
+    if (sender.value != "Одежда" && sender.value != "Обувь" && sender.value != "Домашний интерьер" && sender.value != "Аксессуары"){
+        let parent = sender.parentElement
+        parent.removeChild(parent.getElementsByTagName("input")[1])
+
+        if (parent.children.length == 2) {
+            parent.removeChild(parent.lastElementChild)
+        }
+
+    }
 }
